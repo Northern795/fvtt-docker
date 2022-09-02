c0b667613050d027240d86178a2e6472f82b8a03
Northern795 <30593359+Northern795@users.noreply.github.com>
2022-09-03 00:40:09 +0100

    Update run-server.sh

--- run-server.sh
+++ run-server.sh
@@ -3,7 +3,4 @@
 # look for a file name foundryvtt-.....zip or FoundryVTT...zip, copy it to 
 # /opt/foundryvtt, unzip and remove it. Then start the foundry app
 
-cd /opt/foundryvtt
-find /host -type f -name [f,F]oundry[vtt,VTT]*.zip -exec cp '{}' . ';'
-unzip -o *.zip && rm *.zip
 node resources/app/main.js --dataPath=/data/foundryvtt
